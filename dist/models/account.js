5f1a61484b484cabe73a57847a16ba18a6ea38f1
AnPhamThien <66228093+AnPhamThien@users.noreply.github.com>
2023-03-04 12:13:45 +0700

    init commit

---
+++ dist/models/account.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AccountModel = void 0;
+const mongoose_1 = require("mongoose");
+(0, mongoose_1.connect)('mongodb://localhost/K5-Nodemy');
+const accountSchema = new mongoose_1.Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+}, {
+    collection: 'account'
+});
+const AccountModel = (0, mongoose_1.model)('account', accountSchema);
+exports.AccountModel = AccountModel;
+//# sourceMappingURL=account.js.map
---
+++ src/models/account.ts
@@ -0,0 +1,18 @@
+import { model, Schema, connect, Document } from 'mongoose';
+
+connect('mongodb://localhost/K5-Nodemy');
+
+interface IAccount {
+    username: string;
+    password: string;
+}
+
+const accountSchema: Schema = new Schema<IAccount>({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+}, {
+    collection: 'account'
+});
+
+const AccountModel = model<IAccount>('account', accountSchema)
+export { AccountModel }
